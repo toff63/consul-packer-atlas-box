6346fa82b77a9d813894a8a50b793ab6c284b046
Christophe Marchal <toff63@gmail.com>
2015-12-26 19:55:17 -0200

    updated to install consul

---
+++ scripts/install-consul-ui.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+
+echo Installing dependencies...
+sudo apt-get update
+sudo apt-get install -y unzip curl
+
+echo Fetching Consul UI
+cd /tmp/
+wget https://releases.hashicorp.com/consul/0.6.0/consul_0.6.0_web_ui.zip -O consul-ui.zip
+
+echo Installing UI
+mkdir /home/vagrant/ui
+mv consul-ui.zip /home/vagrant/ui
+cd /home/vagrant/ui
+unzip consul-ui.zip
+chown -R vagrant:vagrant /home/vagrant/ui
+
---
+++ scripts/install-consul.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo Installing dependencies...
+sudo apt-get update
+sudo apt-get install -y unzip curl
+
+echo Fetching Consul...
+cd /tmp/
+# wget https://dl.bintray.com/mitchellh/consul/0.5.2_linux_amd64.zip -O consul.zip
+wget https://releases.hashicorp.com/consul/0.6.0/consul_0.6.0_linux_amd64.zip -O consul.zip
+
+echo Installing Consul...
+unzip consul.zip
+sudo chmod +x consul
+sudo mv consul /usr/bin/consul
+
+sudo mkdir /etc/consul.d
+sudo chmod a+w /etc/consul.d
+
